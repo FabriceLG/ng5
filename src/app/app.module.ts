46e441cdd154338c4f77af67deb45977f23eb29f
fabrice.lg@gmail.com <fabrice.lg@gmail.com>
2018-04-14 20:50:04 +0200

    add page reservation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ReservationsComponent } from './reservations/reservations.component';
+import { CalendarModule } from 'angular-calendar';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { CommonModule, registerLocaleData } from '@angular/common';
+import { UtilsModule } from './utils/utils.module';
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeFr);
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    ReservationsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CalendarModule.forRoot(),
+    NgbModalModule.forRoot(),
+    CommonModule,
+    UtilsModule,
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
